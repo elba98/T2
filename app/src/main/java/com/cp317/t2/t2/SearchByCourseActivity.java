c6b573dcf72714c46f7259c94e7a1e217c9891f3
Rasha Nasri <rasha7695@gmail.com>
2019-11-24 19:28:08 -0500

    Add search by course activity

---
+++ app/src/main/java/com/cp317/t2/t2/SearchByCourseActivity.java
@@ -0,0 +1,55 @@
+package com.cp317.t2.t2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SearchByCourseActivity extends AppCompatActivity {
+    private static final String TAG = "SearchByCourseActivity";
+
+    private ArrayAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_course);
+        ListView list = findViewById(R.id.theList);
+        EditText theFilter = findViewById(R.id.searchFilter);
+        Log.d(TAG, "onCreate: Started.");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("CP368");
+        names.add("MA104");
+        names.add("MA122");
+        names.add("CP104");
+        names.add("CP164");
+        names.add("MA121");
+
+        adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        list.setAdapter(adapter);
+
+        theFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (SearchByCourseActivity.this).adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
+
