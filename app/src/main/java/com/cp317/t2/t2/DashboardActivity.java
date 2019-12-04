71b4d63ab52ffe12b00c0c8626be9a1f8f506f42
elba98 <elba3790@mylaurier.ca>
2019-12-03 20:13:21 -0500

    Tried to query all users in the databse to display ones matching userType="Tutor" if the current user is Tutee, and userType="Tutee" if the current user if Tutor but cant get it to work. You can check it out at like 166 where it is commented out. App works fine still.

--- app/src/main/java/com/cp317/t2/t2/DashboardActivity.java
+++ app/src/main/java/com/cp317/t2/t2/DashboardActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,10 +22,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -37,6 +41,7 @@ public class DashboardActivity extends AppCompatActivity {
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private DatabaseReference usersDatabase;
     private ArrayList<User> userList = new ArrayList<>();
+    private String userType, oppositeUserType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,7 +130,7 @@ public class DashboardActivity extends AppCompatActivity {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     System.out.println(dataSnapshot);
-                    String userType = dataSnapshot.child("userType").getValue(String.class);
+                    userType = dataSnapshot.child("userType").getValue(String.class);
                     String text;
                     if(userType.equals("Tutee")) {
                         text = "Suggested Tutors";
@@ -156,16 +161,41 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void setSuggestedUsers() {
-        User john = new User("elba3790mylaurier.ca", "mitchell", "elbaz", "Tutee",
+        // Get users from database (if logged on user is tutor, get tutees and vise-versa)
+
+//        usersDatabase.orderByChild("userType").equalTo("Tutor").limitToFirst(100).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                System.out.println("amount of users = " + dataSnapshot.getChildrenCount());
+//                for(int i=0; i<dataSnapshot.getChildrenCount(); i++) {
+//                    DataSnapshot child = dataSnapshot.getChildren().iterator().next();
+//                    String fName = child.child("userFirstName").getValue(String.class);
+//                    String lName = child.child("userLirstName").getValue(String.class);
+//                    String program = child.child("program").getValue(String.class);
+//                    User user = new User(fName, lName, program);
+//                    userList.add(user);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
+        User john = new User("elba3790mylaurier.ca", "John", "Tutor", "Tutor",
+                "1234567890", "l1l 1l1");
+        User bob = new User("elba3790mylaurier.ca", "Bob", "Tutee", "Tutee",
                 "1234567890", "l1l 1l1");
         userList.add(john);
+        userList.add(bob);
         user_listView = (ListView) findViewById(R.id.users_listView);
         UserListAdapter adapter = new UserListAdapter(this,R.layout.custom_list, userList);
         user_listView.setAdapter(adapter);
         user_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                System.out.println(i);
                 User user = userList.get(i);
                 Toast.makeText(getApplicationContext(),user.getUserFirstName(),Toast.LENGTH_SHORT).show();
             }
